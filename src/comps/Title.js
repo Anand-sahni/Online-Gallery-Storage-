0216379d11d536507c328a5ed384b60058a1785f
Anand sahni <anandbusiness57@gmail.com>
2020-12-07 08:39:49 +0530

    first commit

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>Online Gallery Storage</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Upload Your Memories on Online Gallery Storage Platform.</p>
     </div>
   )
 }
